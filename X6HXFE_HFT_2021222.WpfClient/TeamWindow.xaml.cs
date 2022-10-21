9ef148679aa8b526d7663a6bf0cab6aedae962ed
feketedominik <feketedominik27@gmail.com>
2022-10-21 14:13:42 +0200

    TeamWindow delete command done

--- X6HXFE_HFT_2021222.WpfClient/TeamWindow.xaml.cs
+++ X6HXFE_HFT_2021222.WpfClient/TeamWindow.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace X6HXFE_HFT_2021222.WpfClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TeamWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TeamWindow : Window
     {
-        public MainWindow()
+        public TeamWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            LeagueWindow lw = new LeagueWindow();
-            lw.ShowDialog();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            TeamWindow tw = new TeamWindow();
-            tw.ShowDialog();
         }
     }
 }
---
+++ X6HXFE_HFT_2021222.WpfClient/ViewModel/TeamWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using X6HXFE_HFT_2021222.Models;
+
+namespace X6HXFE_HFT_2021222.WpfClient.ViewModel
+{
+    public class TeamWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Team> Teams { get; set; }
+
+        private Team selectedTeam;
+
+        public Team SelectedTeam
+        {
+            get { return selectedTeam; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedTeam = new Team()
+                    {
+                        TeamId = value.TeamId,
+                        Name = value.Name
+                    };
+                    OnPropertyChanged();
+                    (DeleteTeamCommand as RelayCommand).NotifyCanExecuteChanged();
+                    //(UpdateLeagueCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+        public ICommand CreateTeamCommand { get; set; }
+        public ICommand UpdateTeamCommand { get; set; }
+        public ICommand DeleteTeamCommand { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+        public TeamWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Teams = new RestCollection<Team>("http://localhost:8739/", "team");
+                DeleteTeamCommand = new RelayCommand(() =>
+                {
+                      Teams.Delete(SelectedTeam.TeamId);
+                },
+                () =>
+                {
+                    return SelectedTeam != null;
+                }
+                );
+                //SelectedTeam = new Team();
+            }
+        }
+    }
+}
