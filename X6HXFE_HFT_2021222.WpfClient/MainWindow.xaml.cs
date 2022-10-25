485fce5afae3e92cdd5084d1f4e2030fd9fc084c
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-25 08:53:29 +0200

    MainWindow edited

--- X6HXFE_HFT_2021222.WpfClient/MainWindow.xaml.cs
+++ X6HXFE_HFT_2021222.WpfClient/MainWindow.xaml.cs
@@ -44,12 +44,6 @@ namespace X6HXFE_HFT_2021222.WpfClient
         {
             PlayerWindow pw = new PlayerWindow();
             pw.ShowDialog();
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            StatisticsWindow sw = new StatisticsWindow();
-            sw.ShowDialog();
-        }
+        }        
     }
 }
