0411682e883e8bb70c3430415a223e4003b2e2d7
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-28 09:46:57 +0200

    team html added

---
+++ X6HXFE_HFT_2021222.JSClient/wwwroot/teamScript.js
@@ -0,0 +1,44 @@
+﻿let teams = [];
+
+fetch('http://localhost:8739/team')
+    .then(x => x.json())
+    .then(y => {
+        teams = y;
+        console.log(y);
+        display();
+    });
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    teams.forEach(t => {
+        document.getElementById('resultarea').innerHTML += 
+            "<tr><td>" + t.teamId + "</td><td>"
+        + t.name + "</td><td>" + t.leagueId + "</td><td>"
+        + t.stadium + "</td><td>" + t.headCoach + "</td><td>"
+        + t.founded + "</td><td>" +
+            `<button type="button" onclick="remove(${t.teamId})">Delete </button>` +
+            `<button type="button" onclick="showupdate(${t.teamid})">Update </button>`
+            + "</td></tr>";
+    })
+}
+
+function remove(id) {
+    alert(id);
+}
+
+function create() {
+    let name = document.getElementById('teamname').value;
+    fetch('http://localhost:8739/team', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { name: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+    display();
+}
